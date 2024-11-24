659f1dfb28e4830535f194aeaa666dfcfc165c49
Alexander Retamero <alexandreretamero@gmail.com>
2024-11-24 19:46:51 -0300

    feat: implement SessionService and update SessionModule providers

--- src/modules/session/session.module.ts
+++ src/modules/session/session.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { SessionService } from '@/modules/session/session.service';
 
 @Module({
-  providers: [],
+  providers: [SessionService],
 })
 export class SessionModule {}
