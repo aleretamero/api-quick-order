00d0ccb3d3e030f08672cc6cb3a9127540e37d19
Alexander Retamero <alexandreretamero@gmail.com>
2024-11-25 10:16:44 -0300

    feat: integrate PrismaModule into DeviceModule for improved database interactions

--- src/modules/device/device.module.ts
+++ src/modules/device/device.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DeviceService } from '@/modules/device/device.service';
+import { PrismaModule } from '@/infra/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [DeviceService],
+  exports: [DeviceService],
 })
 export class DeviceModule {}
