3b7270d49f96bec106469578dadddfc7221975f2
Alexander Retamero <alexandreretamero@gmail.com>
2024-11-25 11:35:03 -0300

    feat: implement JwtService with sign and verify methods, and add Issuer enum

--- src/infra/jwt/jwt.module.ts
+++ src/infra/jwt/jwt.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { JwtModule as DefaultJwtModule } from '@nestjs/jwt';
+import { JwtService } from '@/infra/jwt/jwt.service';
 
-@Module({})
+@Module({
+  imports: [DefaultJwtModule],
+  providers: [JwtService],
+  exports: [JwtService],
+})
 export class JwtModule {}
---
+++ src/infra/jwt/jwt.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService as DefaultJwtService } from '@nestjs/jwt';
+import { Issuer } from '@/infra/jwt/enums/issue.enum';
+
+type OffsetString = `${number}${'s' | 'm' | 'h' | 'd'}`;
+
+export type SignJwtOptions<T extends object | Buffer = any> = {
+  expiresIn?: OffsetString;
+  issuer?: Issuer;
+  subject?: string;
+} & {
+  secret: string;
+  payload: T;
+};
+
+export type VerifyJwtOptions = {
+  token: string;
+  secret: string;
+  issuer?: Issuer;
+  subject?: string;
+};
+
+export type JwtPayload<T extends object | Buffer = any> = {
+  iat?: number;
+  exp?: number;
+  iss?: string;
+  sub?: string;
+} & T;
+
+@Injectable()
+export class JwtService {
+  constructor(private readonly jwtService: DefaultJwtService) {}
+
+  async sign<T extends object | Buffer>(
+    options: SignJwtOptions<T>,
+  ): Promise<string> {
+    return this.jwtService.sign(options.payload ?? {}, {
+      issuer: options.issuer,
+      subject: options.subject,
+      expiresIn: options.expiresIn,
+      secret: options.secret,
+    });
+  }
+
+  async verify<T extends object>(
+    options: VerifyJwtOptions,
+  ): Promise<JwtPayload<T>> {
+    return this.jwtService.verify<T>(options.token, {
+      secret: options.secret,
+      issuer: options.issuer,
+      subject: options.subject,
+    });
+  }
+}
