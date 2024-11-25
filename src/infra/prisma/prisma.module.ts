07347d34b869b6013fee3cee7962a0f0f3d8c84f
Alexander Retamero <alexandreretamero@gmail.com>
2024-11-25 10:21:29 -0300

    refactor: remove global decorator from PrismaModule

--- src/infra/prisma/prisma.module.ts
+++ src/infra/prisma/prisma.module.ts
@@ -1,7 +1,6 @@
-import { Global, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { PrismaService } from '@/infra/prisma/prisma.service';
 
-@Global()
 @Module({
   providers: [PrismaService],
   exports: [PrismaService],
