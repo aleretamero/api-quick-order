83c7bcd15c58ce4567fd6402a22fd305d65b723e
Alexander Retamero <alexandreretamero@gmail.com>
2024-11-25 09:40:20 -0300

    feat: implement upsert method in DeviceService for device management

--- src/modules/device/device.service.ts
+++ src/modules/device/device.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '@/infra/prisma/prisma.service';
+import { DevicePresenter } from '@/modules/device/presenters/device.presenter';
 
 @Injectable()
-export class DeviceService {}
+export class DeviceService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async upsert(userId: string, fingerprint: string): Promise<DevicePresenter> {
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        id: userId,
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException(`User not found with id: ${userId}`); // TODO: i18n
+    }
+
+    const device = await this.prismaService.device.upsert({
+      where: {
+        fingerprint_userId: {
+          fingerprint,
+          userId: user.id,
+        },
+      },
+      update: {},
+      create: {
+        userId: user.id,
+        fingerprint,
+      },
+    });
+
+    return new DevicePresenter(device);
+  }
+}
