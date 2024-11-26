0f444881eede43efb69cbc80c9dc61371a795063
Alexander Retamero <alexandreretamero@gmail.com>
2024-11-26 11:04:34 -0300

    feat: remove AppController and AppService

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,12 @@ import { APP_GUARD } from '@nestjs/core';
 import { Module } from '@nestjs/common';
 import { JwtAuthGuard } from '@/modules/auth/guards/jwt-auth.guard';
 import { AuthModule } from '@/modules/auth/auth.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaModule } from '@/infra/prisma/prisma.module';
 import { JwtModule } from '@/infra/jwt/jwt.module';
 import { EnvModule } from '@/infra/env/env.module';
 
 @Module({
   imports: [AuthModule, PrismaModule, JwtModule, EnvModule],
-  controllers: [AppController],
-  providers: [AppService, { provide: APP_GUARD, useClass: JwtAuthGuard }],
+  providers: [{ provide: APP_GUARD, useClass: JwtAuthGuard }],
 })
 export class AppModule {}
