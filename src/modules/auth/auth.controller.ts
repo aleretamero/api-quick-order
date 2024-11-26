e02ea69ca132a23daf29f14a930a7d1c336dbdfa
Alexander Retamero <alexandreretamero@gmail.com>
2024-11-26 09:03:11 -0300

    feat: add IsPublic decorator to login route in AuthController

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,12 +3,14 @@ import { AuthService } from '@/modules/auth/auth.service';
 import { SessionPresenter } from '@/modules/session/presenters/session.presenter';
 import { LoginDto } from '@/modules/auth/dtos/login.dto';
 import { AuthenticateDto } from '@/modules/auth/dtos/authenticate.dto';
+import { IsPublic } from '@/common/decorators/is-public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
+  @IsPublic()
   login(
     @Body() body: LoginDto,
     @Headers() headers: AuthenticateDto,
