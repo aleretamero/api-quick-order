d2e7217728bc2062e31313789617d366182aad7e
Alexander Retamero <alexandreretamero@gmail.com>
2024-11-26 10:14:39 -0300

    feat: enhance AuthController with API documentation and HTTP status codes

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Headers, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Headers,
+  HttpCode,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
 import { AuthService } from '@/modules/auth/auth.service';
 import { SessionPresenter } from '@/modules/session/presenters/session.presenter';
 import { LoginDto } from '@/modules/auth/dtos/login.dto';
@@ -6,6 +14,7 @@ import { AuthenticateDto } from '@/modules/auth/dtos/authenticate.dto';
 import { IsPublic } from '@/common/decorators/is-public.decorator';
 import { CurrentUser } from '@/common/decorators/current-user.decorator';
 import { UserPresenter } from '@/modules/user/presenters/user.presenter';
+import { ApiDocs } from '@/common/decorators/api-docs.decorators';
 
 @Controller('auth')
 export class AuthController {
@@ -13,6 +22,8 @@ export class AuthController {
 
   @Post('login')
   @IsPublic()
+  @HttpCode(HttpStatus.OK)
+  @ApiDocs({ isPublic: true, response: [400, 401, 500] })
   login(
     @Body() body: LoginDto,
     @Headers() headers: AuthenticateDto,
@@ -21,6 +32,7 @@ export class AuthController {
   }
 
   @Get('me')
+  @ApiDocs({ isPublic: true, response: [401, 500] })
   me(@CurrentUser('id') userId: string): Promise<UserPresenter> {
     return this.authService.me(userId);
   }
