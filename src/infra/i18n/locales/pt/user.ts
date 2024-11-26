40cf5fb0ccc1710e4b5af25b377da4fc303202ed
Alexander Retamero <alexandreretamero@gmail.com>
2024-11-26 09:29:45 -0300

    feat: add 'not_found' message to Portuguese localization for user

--- src/infra/i18n/locales/pt/user.ts
+++ src/infra/i18n/locales/pt/user.ts
@@ -1,5 +1,6 @@
 import { UserI18n } from '@/infra/i18n/protocols/user-i18n';
 
 export default {
+  not_found: 'Usuário não encontrado',
   not_found_with_id: 'Usuário não encontrado com id: {userId}',
 } satisfies UserI18n;
--- src/infra/i18n/protocols/user-i18n.ts
+++ src/infra/i18n/protocols/user-i18n.ts
@@ -1,3 +1,4 @@
 export interface UserI18n {
+  not_found: string;
   not_found_with_id: string;
 }
