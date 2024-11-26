3e3a3eef9569a55c3ea5bb1094a745e4687fee8e
Alexander Retamero <alexandreretamero@gmail.com>
2024-11-26 09:01:50 -0300

    feat: integrate JwtAuthGuard and additional modules into AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
+import { APP_GUARD } from '@nestjs/core';
 import { Module } from '@nestjs/common';
+import { JwtAuthGuard } from '@/modules/auth/guards/jwt-auth.guard';
+import { AuthModule } from '@/modules/auth/auth.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from '@/modules/auth/auth.module';
+import { PrismaModule } from '@/infra/prisma/prisma.module';
+import { JwtModule } from '@/infra/jwt/jwt.module';
+import { EnvModule } from '@/infra/env/env.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, PrismaModule, JwtModule, EnvModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, { provide: APP_GUARD, useClass: JwtAuthGuard }],
 })
 export class AppModule {}
