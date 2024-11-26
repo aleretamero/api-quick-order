77716a9b3b2ee3eb4acdd79c2738dd60b63bff47
Alexander Retamero <alexandreretamero@gmail.com>
2024-11-25 23:18:31 -0300

    feat: add 'me' endpoint to AuthController for user session retrieval

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Headers, Post } from '@nestjs/common';
+import { Body, Controller, Get, Headers, Post } from '@nestjs/common';
 import { AuthService } from '@/modules/auth/auth.service';
 import { SessionPresenter } from '@/modules/session/presenters/session.presenter';
 import { LoginDto } from '@/modules/auth/dtos/login.dto';
@@ -14,5 +14,10 @@ export class AuthController {
     @Headers() headers: AuthenticateDto,
   ): Promise<SessionPresenter> {
     return this.authService.login(body, headers);
+  }
+
+  @Get('me')
+  me() {
+    return 'me';
   }
 }
