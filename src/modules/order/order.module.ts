43d268db73c5b405e390eb4343118e3e99c5c3aa
Alexander Retamero <alexandreretamero@gmail.com>
2024-11-27 13:19:23 -0300

    feat: add OrderController to OrderModule

--- src/modules/order/order.module.ts
+++ src/modules/order/order.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { OrderController } from '@/modules/order/order.controller';
 
-@Module({})
+@Module({
+  controllers: [OrderController],
+})
 export class OrderModule {}
