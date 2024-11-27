fbb6f466589e8bd4b2ef717bc59d55d5995ba299
Alexander Retamero <alexandreretamero@gmail.com>
2024-11-27 14:05:39 -0300

    feat: add role-based access control and API documentation for create order endpoint

--- src/modules/order/order.controller.ts
+++ src/modules/order/order.controller.ts
@@ -2,12 +2,17 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { OrderService } from '@/modules/order/order.service';
 import { CreateOrderDto } from '@/modules/order/dtos/create-order.dto';
 import { OrderPresenter } from '@/modules/order/presenters/order.presenter';
+import { Roles } from '@/common/decorators/roles.decorator';
+import { Role } from '@/modules/user/enums/role.enum';
+import { ApiDocs } from '@/common/decorators/api-docs.decorators';
 
 @Controller('orders')
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
 
   @Post()
+  @Roles(Role.ADMIN)
+  @ApiDocs({ response: [400, 401, 403, 500] })
   create(@Body() body: CreateOrderDto): Promise<OrderPresenter> {
     return this.orderService.create(body);
   }
