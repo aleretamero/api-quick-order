437190369edb9ca670f4395b4b161f7f03ca5448
Alexander Retamero <alexandreretamero@gmail.com>
2024-11-27 13:20:50 -0300

    feat: add OrderService to OrderModule and create OrderService class

--- src/modules/order/order.module.ts
+++ src/modules/order/order.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { OrderController } from '@/modules/order/order.controller';
+import { OrderService } from '@/modules/order/order.service';
 
 @Module({
   controllers: [OrderController],
+  providers: [OrderService],
 })
 export class OrderModule {}
