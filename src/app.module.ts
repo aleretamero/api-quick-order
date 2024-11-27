8ed0216731fa38c5e2d44e0b5007cf024847c744
Alexander Retamero <alexandreretamero@gmail.com>
2024-11-27 13:18:34 -0300

    feat: add OrderModule to the application and include it in the main module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,17 @@ import { JwtModule } from '@/infra/jwt/jwt.module';
 import { EnvModule } from '@/infra/env/env.module';
 import { RolesGuard } from '@/modules/auth/guards/roles.guard';
 import { I18nModule } from '@/infra/i18n/i18n-module';
+import { OrderModule } from '@/modules/order/order.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, I18nModule, JwtModule, EnvModule],
+  imports: [
+    PrismaModule,
+    I18nModule,
+    JwtModule,
+    EnvModule,
+    AuthModule,
+    OrderModule,
+  ],
   providers: [
     { provide: APP_GUARD, useClass: JwtAuthGuard },
     { provide: APP_GUARD, useClass: RolesGuard },
