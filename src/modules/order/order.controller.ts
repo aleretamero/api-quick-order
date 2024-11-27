7c54dd5c4097cbce280b6f0be024673f01ce3377
Alexander Retamero <alexandreretamero@gmail.com>
2024-11-27 13:31:05 -0300

    feat: add create method to OrderService and implement POST endpoint in OrderController

--- src/modules/order/order.controller.ts
+++ src/modules/order/order.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { OrderService } from '@/modules/order/order.service';
 
 @Controller('orders')
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
+
+  @Post()
+  create() {
+    return this.orderService.create();
+  }
 
   @Get()
   index() {
--- src/modules/order/order.service.ts
+++ src/modules/order/order.service.ts
@@ -5,6 +5,10 @@ import { PrismaService } from '@/infra/prisma/prisma.service';
 export class OrderService {
   constructor(private readonly prismaService: PrismaService) {}
 
+  create() {
+    throw new Error('Method not implemented.');
+  }
+
   async findAll() {
     return this.prismaService.order.findMany();
   }
