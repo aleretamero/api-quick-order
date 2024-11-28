413edf85fdcedc228d74b5ca18a5b4258be1f68c
Alexander Retamero <alexandreretamero@gmail.com>
2024-11-28 13:16:18 -0300

    feat: add StorageFirebaseService and update StorageFirebaseModule for service integration

--- src/infra/storage-firebase/storage-firebase.module.ts
+++ src/infra/storage-firebase/storage-firebase.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { StorageFirebaseService } from '@/infra/storage-firebase/storage-firebase.service';
 
-@Module({})
+@Module({
+  providers: [StorageFirebaseService],
+  exports: [StorageFirebaseService],
+})
 export class StorageFirebaseModule {}
