93ccf59118ccc634c45f9c2bb848ef3fa774909b
Alexander Retamero <alexandreretamero@gmail.com>
2024-11-28 09:23:38 -0300

    feat: integrate I18nModule and implement update method in OrderService with localization support

--- src/modules/order/order.module.ts
+++ src/modules/order/order.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { OrderController } from '@/modules/order/order.controller';
 import { OrderService } from '@/modules/order/order.service';
 import { PrismaModule } from '@/infra/prisma/prisma.module';
+import { I18nModule } from '@/infra/i18n/i18n-module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, I18nModule],
   controllers: [OrderController],
   providers: [OrderService],
 })
--- src/modules/order/order.service.ts
+++ src/modules/order/order.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '@/infra/prisma/prisma.service';
+import { I18nService } from '@/infra/i18n/i18n-service';
 import { OrderStatus } from '@/modules/order/enums/order-status.enum';
 import { CreateOrderDto } from '@/modules/order/dtos/create-order.dto';
+import { UpdateOrderDto } from '@/modules/order/dtos/update-order.dto';
 import { OrderPresenter } from '@/modules/order/presenters/order.presenter';
 import { Role } from '@/modules/user/enums/role.enum';
 import { PaginationPresenter } from '@/common/presenters/pagination.presenter';
@@ -10,7 +12,10 @@ import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class OrderService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly i18nService: I18nService,
+  ) {}
 
   async create(
     sessionId: string,
@@ -76,6 +81,55 @@ export class OrderService {
         currentPage: page,
         itemsPerPage: limit,
       },
+    });
+  }
+
+  async update(
+    sessionId: string,
+    orderId: string,
+    dto: UpdateOrderDto,
+  ): Promise<OrderPresenter> {
+    const currentOrder = await this.prismaService.order.findUnique({
+      where: {
+        id: orderId,
+      },
+    });
+
+    if (!currentOrder) {
+      throw new NotFoundException(
+        this.i18nService.t('order.not_found_with_id', { orderId }),
+      );
+    }
+
+    const order = await this.prismaService.$transaction(async (ctx) => {
+      const updatedOrder = await ctx.order.update({
+        where: {
+          id: orderId,
+        },
+        data: {
+          description: dto.description,
+          salePrice: dto.salePrice,
+          receivedPrice: dto.receivedPrice,
+          image: dto.image,
+          status: dto.status,
+        },
+      });
+
+      await ctx.orderLog.create({
+        data: {
+          sessionId: sessionId,
+          orderId: orderId,
+          beforeState: currentOrder,
+          afterState: updatedOrder,
+        },
+      });
+
+      return updatedOrder;
+    });
+
+    return new OrderPresenter({
+      ...order,
+      isAdmin: true,
     });
   }
 }
