5c130e5cbe1a6ea32fa28b40fe8a87f6c17857e1
Alexander Retamero <alexandreretamero@gmail.com>
2024-11-28 13:09:12 -0300

    feat: import EnvModule in StorageLocalModule for environment configuration

--- src/infra/storage-local/storage-local.module.ts
+++ src/infra/storage-local/storage-local.module.ts
@@ -1,7 +1,9 @@
-import { StorageLocalService } from '@/infra/storage-local/storage-local.service';
 import { Module } from '@nestjs/common';
+import { EnvModule } from '@/infra/env/env.module';
+import { StorageLocalService } from '@/infra/storage-local/storage-local.service';
 
 @Module({
+  imports: [EnvModule],
   providers: [StorageLocalService],
   exports: [StorageLocalService],
 })
