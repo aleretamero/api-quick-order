9943a49fba2ab6ef268e6b9ba613d82aca593039
Alexandre Retamero <alexandreretamero@gmail.com>
2024-12-05 10:34:22 -0300

    feat: add ReportService and update ReportModule to include it

--- src/modules/reports/report.module.ts
+++ src/modules/reports/report.module.ts
@@ -1,7 +1,9 @@
-import { ReportController } from '@/modules/reports/report.controller';
 import { Module } from '@nestjs/common';
+import { ReportController } from '@/modules/reports/report.controller';
+import { ReportService } from '@/modules/reports/report.service';
 
 @Module({
   controllers: [ReportController],
+  providers: [ReportService],
 })
 export class ReportModule {}
